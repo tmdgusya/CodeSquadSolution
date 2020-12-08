e2ac53852beef4806fa3bf0a56f14b631164c202
roach <tmdgusya@naver.com>
2020-12-08 15:55:38 +0900

    주요 기능 사항 작성완료 * Step2.java : roationcube 작성완료 * Step2.java : showCube 작성완료 * Description 오후에 작성예정 * 현제 테스트 불가로, 집가서 테스트 해보아야 함. Run 이 안됨.

--- Step-2/Step2.java
+++ Step-2/Step2.java
@@ -1,12 +1,13 @@
+import java.util.Scanner;
 class Step2 {
 
     public static void validateKey(String key) throws CustomError{
-        if(!(key.equals("U") || key.equals("U`") || key.equals("R") || key.equals("R`") || key.equals("L") || key.equals("L`") || key.equals("B") || key.equals("B`") || key.equals("Q"))){
+        if(!(key.equals("U") || key.equals("U`") || key.equals("R") || key.equals("R`") || key.equals("L") || key.equals("L`") || key.equals("B") || key.equals("B`"))){
             throw new CustomError("올바른 키를 입력하지 않았습니다. 프로그램이 종료됩니다.");
         }
     }
 
-    public static String[][] rotationCube(String[][] cube, String key) throws CustomError{
+    public static String[][] rotationCube(String[][] cube, String key){
         String[][] tempCube = deepClone(cube);
         try{
             validateKey(key);
@@ -14,6 +15,16 @@ class Step2 {
             System.out.println(er.getMessage());
         }
         switch (key){
+            case "U":
+            for(int i = cube[0].length-1; i>=0; i--){
+                int index = i-1;
+                if(index >= 0){
+                    tempCube[0][index] = cube[0][i];
+                }else{
+                    tempCube[0][cube[0].length-1] = cube[0][0];
+                } 
+            }
+            break;
             case "U`" :
                 for(int i = 0; i<cube[0].length; i++){
                     int index = i+1;
@@ -24,67 +35,70 @@ class Step2 {
                     }
                 }
                 break;
-            case "U":
-                for(int i = 0; i<cube[0].length; i++){
-                    int index = i+1;
-                    if(index == cube[0].length-1){
-                        tempCube[0][cube[0].length-2] = cube[0][cube[0].length-1];
-                    }else{
-                        tempCube[0][i] = cube[0][index];
+            case "R":
+                int right_index = cube[0].length;
+                for(int i = cube.length-1; i>0; i--){
+                    int index = i-1;
+                    if(index <= cube.length){
+                        tempCube[index][right_index] = cube[i][right_index];
                     }
                 }
-                break;
-            case "R":
-
+                tempCube[cube.length-1][right_index] = cube[0][right_index];
                 break;
             case "R`":
-
-                break;
-            case "L":
-                for(int i = 0; i<cube.length; i++){
+                right_index = cube[0].length;
+                for(int i = 0 ; i<cube.length-1; i++){
                     int index = i+1;
-                    if(index == cube.length-1){
-                        tempCube[0][0] = cube[cube.length-1][0];
-                    }else{
+                    if(index <= cube.length-1){
+                        tempCube[index][right_index] = cube[i][right_index];
+                    }
+                }
+                tempCube[0][right_index] = cube[cube.length-1][right_index];
+                break;
+            case "L": 
+                for(int i = 0 ; i<cube.length-1; i++){
+                    int index = i+1;
+                    if(index <= cube.length-1){
                         tempCube[index][0] = cube[i][0];
                     }
                 }
+                tempCube[0][0] = cube[cube.length-1][0];
                 break;
             case "L`":
-                for(int i = 0; i<cube.length; i++){
-                    int index = i+1;
-                    if(index == cube.length-1){
-                        tempCube[0][0] = cube[cube.length-1][0];
-                    }else{
-                        tempCube[i][0] = cube[index][0];
+                for(int i = cube.length-1; i>0; i--){
+                    int index = i-1;
+                    if(index <= cube.length){
+                        tempCube[index][0] = cube[i][0];
                     }
                 }
+                tempCube[cube.length-1][0] = cube[0][0];
                 break;
             case "B":
-                for(int i = 0; i<cube[0].length; i++){
+                int bottom_index = cube.length-1;
+                for(int i = 0; i<cube[bottom_index].length; i++){
                     int index = i+1;
-                    if(index == cube[0].length-1){
-                        tempCube[cube.length-1][0] = cube[cube.length-1][i];
+                    if(index == cube[bottom_index].length){
+                        tempCube[bottom_index][0] = cube[bottom_index][(cube[0].length-1)];
                     }else{
-                        tempCube[cube.length-1][index] = cube[cube.length-1][i];
+                        tempCube[bottom_index][index] = cube[bottom_index][i];
                     }
                 }
                 break;
             case "B`":
-                for(int i = 0; i<cube[0].length; i++){
-                    int index = i+1;
-                    if(index == cube[0].length-1){
-                        tempCube[cube.length-1][0] = cube[cube.length-1][i];
+                bottom_index = cube.length-1;
+                for(int i = cube[0].length-1; i>=0; i--){
+                    int index = i-1;
+                    if(index >= 0){
+                        tempCube[bottom_index][index] = cube[bottom_index][i];
                     }else{
-                        tempCube[cube.length-1][i] = cube[cube.length-1][index];
-                    }
+                        tempCube[bottom_index][cube[0].length-1] = cube[bottom_index][0];
+                    } 
                 }
                 break;
-            case "Q":
-                throw new CustomError("프로그램을 종료합니다.");
         }
         return tempCube;
     }
+
     private static String[][] deepClone(String[][] array){
         String[][] cloneArray = new String[array.length][array[0].length];
         for(int i=0; i<array.length; i++) {
@@ -94,21 +108,33 @@ class Step2 {
         }
         return cloneArray;
     }
+
+    public static void showCube(String[][] array){
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j <array[0].length; j++){
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+
     public static void main(String[] args) {
-
+        
         String[][] Example = {{"R","R","W"},{"G","C","W"},{"G","B","B"}};
-        try {
-            Example = rotationCube(Example, "U");
-        }catch (Exception e){
-            e.getMessage();
-        }
-        for(int i = 0; i<Example[0].length; i++){
-            System.out.println(Example[0][i] + " ");
-        }
-
-
-
-
-
+        Scanner scan = new Scanner(System.in);
+        String action = "";
+        while(true){
+            System.out.println("움직일 방향을 입력해 주십시오 : ");
+            action += scan.nextLine();
+            if(action.equals("Q") || action.equals("q")){
+                System.out.println("프로그램을 종료합니다.");
+                scan.close();
+                break;
+            }else{
+                Example = rotationCube(Example, action);
+                System.out.println();
+                showCube(Example);
+            }
+        }    
     }
 }
